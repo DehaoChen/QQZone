e9484e18b9460ec4e9bef3d9c7102d3916297fab
chendehao <958689927@qq.com>
2016-08-12 22:33:58 +0800

    添加底部自定义的控件 BottomMenuView

---
+++ QQZone/Classes/Views/CDH_BottomMenuView.swift
@@ -0,0 +1,21 @@
+//
+//  CDH_BottomMenuView.swift
+//  QQZone
+//
+//  Created by chendehao on 16/8/12.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+import UIKit
+
+class CDH_BottomMenuView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
