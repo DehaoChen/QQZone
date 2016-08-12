1645b1e63f714b6eb309f9fc5b1478409c5b7f71
chendehao <958689927@qq.com>
2016-08-12 22:35:00 +0800

    添加头像 Button --自定义为 IconButton

---
+++ QQZone/Classes/Views/CDH_IconButton.swift
@@ -0,0 +1,21 @@
+//
+//  CDH_IconButton.swift
+//  QQZone
+//
+//  Created by chendehao on 16/8/12.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+import UIKit
+
+class CDH_IconButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
