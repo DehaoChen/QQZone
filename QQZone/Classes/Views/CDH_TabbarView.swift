cfae586a836c80f19cbd501d32b2d587f81f27ad
chendehao <958689927@qq.com>
2016-08-12 22:35:56 +0800

    添加自定义的 TabbarView

---
+++ QQZone/Classes/Views/CDH_TabbarView.swift
@@ -0,0 +1,21 @@
+//
+//  CDH_TabbarView.swift
+//  QQZone
+//
+//  Created by chendehao on 16/8/12.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+import UIKit
+
+class CDH_TabbarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
